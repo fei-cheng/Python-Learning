ba684da6b163d4b31979e8b97fd48a1f2e123c80
Chengfei <chengfei0913@gmail.com>
2013-07-12 15:30:23 +0800

    spaces replace

---
+++ 1.5-spaceReplace.py
@@ -0,0 +1,9 @@
+﻿"""
+    Write a method to replace all spaces in a string with ‘%20’.
+"""
+
+def spaceReplace(st):
+    return st.replace(' ', '%20')
+
+
+print spaceReplace('Life is short, I use Python.')
