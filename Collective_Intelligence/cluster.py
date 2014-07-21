27f5204cd8c1c5cf462ff5256136a6b875fd0123
Chengfei <chengfei0913@gmail.com>
2014-07-21 14:02:32 +0800

    Viewing data in two dimensions

--- Collective_Intelligence/cluster.py
+++ Collective_Intelligence/cluster.py
@@ -215,8 +215,74 @@ def kcluster(rows, distance=pearson, k=4):
     return bestmatches
         
     
+def tanamoto(v1, v2):
+    c1, c2, shr = 0, 0, 0
+    for i in range(len(v1)):
+        if v1[i] != 0:
+            c1 += 1       # in v1
+        if v2[i] != 0:
+            c2 += 1       # in v2
+        if v1[i] != 0 and v2[i] != 0:
+            shr += 1      # in both
+    return 1.0 - (float(shr) / (c1+c2-shr))
+  
+  
+def scaledown(data, distance=pearson, rate=0.01):
+    n = len(data)
+    outersum = 0.0
+    
+    fakedist = [[0.0 for j in range(n)] for i in range(n)]
+    realdist = [[distance(data[i], data[j]) for j in range(n)] for i in range(n)]
+    
+    loc = [[random.random(), random.random()] for i in range(n)]
+    
+    lasterror = None
+    for m in range(0, 1000):
+        # Find projected distances
+        for i in range(n):
+            for j in range(i, n):
+                fakedist[i][j] = sqrt(sum([pow(loc[i][idx] - loc[j][idx], 2) for idx in range(len(loc[i]))]))
+                fakedist[j][i] = fakedist[i][j]
+        # Move points
+        grad = [[0.0, 0.0] for i in range(n)]
         
+        totalerror = 0
+        for k in range(n):
+            for j in range(n):
+                if j == k:
+                    continue
+                # The error is percent difference between the distances
+                errorterm = (fakedist[j][k] - realdist[j][k]) / realdist[j][k]
+                
+                grad[k][0] += ( (loc[k][0] - loc[j][0]) / fakedist[j][k]) * errorterm
+                grad[k][1] += ( (loc[k][1] - loc[j][1]) / fakedist[j][k]) * errorterm
+                
+                totalerror += abs(errorterm)
+        print totalerror
+        
+        # If the answer got worse by moving the points, we are done
+        if lasterror and lasterror < totalerror:
+            break
+            
+        # Move each of the points by the learning rate times the gradient
+        for k in range(n):
+            loc[k][0] -= rate * grad[k][0]
+            loc[k][1] -= rate * grad[k][1]
+    
+    return loc
+  
+  
+def draw2d(data, labels, jpeg='mds2d.jpg'):
+    img = Image.new('RGB', (2000,2000), (255,255,255))
+    draw = ImageDraw.Draw(img)
+    for i in range(len(data)):
+        x = (data[i][0] + 0.5) * 1000
+        y = (data[i][1] + 0.5) * 1000
+        draw.text((x,y), labels[i], (0,0,0))
+    img.save(jpeg, 'JPEG')
+
+  
 if __name__ == '__main__':
-    rownames, colnames, data = readfile('blogdata.txt')
-    bestmatches = kcluster(data, k=10)
-    print bestmatches
+    blognames, words, data = readfile('blogdata.txt')
+    coords = scaledown(data)
+    draw2d(coords, blognames, jpeg='blogs2d.jpg')
